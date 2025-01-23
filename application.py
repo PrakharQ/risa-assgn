36fea7553cb538bb66f5d36e845fb01f96d815a5
prakhsri <prakhar@borneo.io>
2025-01-23 23:02:52 +0530

    Updating requirements file

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ FACEBOOK_APP_ID = os.getenv("FACEBOOK_APP_ID")
 FACEBOOK_APP_SECRET = os.getenv("FACEBOOK_APP_SECRET")
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-REDIRECT_URI = os.getenv("REDIRECT_URI", "http://localhost:8000/api/callback")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
 UPLOAD_BUCKET = os.getenv("UPLOAD_BUCKET")
 
 
